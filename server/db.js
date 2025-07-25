91c7576ba986b0904f3f17629026f2e145f37ce8
michael-lfc <michelino.realmike@gmail.com>
2025-07-25 14:39:03 +0100

    recent commit

--- server/db.js
+++ server/db.js
@@ -1,38 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectToDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.DB_URI);
-//     console.log("[database]: Connected to MongoDB");
-//   } catch (error) {
-//     console.error("[database]: Connection failed", error);
-//     throw new Error("Database connection failed");
-//   }
-// };
-
-// import mongoose from "mongoose";
-
-// let isConnected = false;
-
-// const connectToDB = async () => {
-//   if (isConnected) return;
-
-//   try {
-//     await mongoose.connect(process.env.DB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     });
-
-//     isConnected = true;
-//     console.log("[database]: Connected to MongoDB");
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//     throw error;
-//   }
-// };
-
-// export default connectToDB;
-
 import mongoose from "mongoose";
 
 let isConnected = false;
