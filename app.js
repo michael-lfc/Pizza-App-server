2c50bba6207816795b1d8d54382f6abc9e694168
michael-lfc <michelino.realmike@gmail.com>
2025-07-28 07:37:15 +0100

    modify app.js

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ import cors from "cors";
 
 const app = express();
 
-const FRONTEND_ORIGIN = "http://localhost:5173"; // Replace with your frontend URL in production
+// const FRONTEND_ORIGIN = "http://localhost:5173"; // Replace with your frontend URL in production
 
-app.use(cors({
-  origin: FRONTEND_ORIGIN,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: FRONTEND_ORIGIN,
+//   credentials: true,
+// }));
+// app.use(cors())
 
 app.use(express.json());
+app.use(cors())
 
 // example route
 app.get("/api/pizzas", (req, res) => {
